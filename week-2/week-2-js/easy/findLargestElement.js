6379a2b54963f7d7acd96bada38bf6396fb93bf3
Pratham Yadav <iiprathamyadav@gmail.com>
2026-04-24 23:00:22 +0530

    just a check

--- week-2/week-2-js/easy/findLargestElement.js
+++ week-2/week-2-js/easy/findLargestElement.js
@@ -7,13 +7,7 @@
 
 function findLargestElement(numbers) {
     
-    let largest = numbers[0];
     
-    for( let i = 1 ; i < numbers.length ; i++ ){
-        if ( largest < numbers[i] )
-            largest = numbers[i];
-    };
-    return largest ;
 }
 
 module.exports = findLargestElement;
